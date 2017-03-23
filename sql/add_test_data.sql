aa974ea49a5a7604d0aff3f7dc74f35ccdd21ce2
samuvait <samu.vaittinen@helsinki.fi>
2017-03-23 22:44:52 +0200

    testidata

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,5 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (name, password) VALUES ('Pekka', 'Pekka123');
+INSERT INTO Kayttaja (name, password) VALUES ('Maija', 'Maija123');
+
+INSERT INTO Askare(name, description, lisatty, deadline, tarkeysaste) VALUES ('Tee tiskit', 'Yhdessä aiheutettujen tiskien tiskaaminen käsin. Luo harmoniaa.', NOW(), '2017-30-03', 1);
