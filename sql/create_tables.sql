418be9feb4cbd9dd1e780522009aa4df79413705
samuvait <samu.vaittinen@helsinki.fi>
2017-03-23 23:19:48 +0200

    jossain virhe

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,23 +1,6 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
--- CREATE TABLE Kayttaja(
--- id SERIAL PRIMARY KEY,
--- name varchar(50) NOT NULL,
--- password varchar(50) NOT NULL
--- );
--- 
--- CREATE TABLE Askare(
--- id SERIAL PRIMARY KEY,
--- kayttaja_id INTEGER REFERENCES Kayttaja(id),
--- name varchar(50) NOT NULL,
--- suoritettu boolean DEFAULT FALSE,
--- description varchar(400),
--- lisatty DATE,
--- deadline DATE,
--- tarkeysaste INTEGER
--- );
 CREATE TABLE Player(
-  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
-  name varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
   password varchar(50) NOT NULL
 );
 
@@ -30,3 +13,4 @@ CREATE TABLE Game(
   published DATE,
   publisher varchar(50),
   added DATE
+);
