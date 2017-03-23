a68c838b023f902b51b5dc4acdb3bcf7093075ba
samuvait <samu.vaittinen@helsinki.fi>
2017-03-23 23:11:59 +0200

    kokeillaan esimerkilla

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,5 +1,10 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
-INSERT INTO Kayttaja (name, password) VALUES ('Pekka', 'Pekka123');
-INSERT INTO Kayttaja (name, password) VALUES ('Maija', 'Maija123');
-
-INSERT INTO Askare(name, description, lisatty, deadline, tarkeysaste) VALUES ('Tee tiskit', 'Yhdessä aiheutettujen tiskien tiskaaminen käsin. Luo harmoniaa.', NOW(), '2017-30-03', 1);
+-- INSERT INTO Kayttaja (name, password) VALUES ('Pekka', 'Pekka123');
+-- INSERT INTO Kayttaja (name, password) VALUES ('Maija', 'Maija123');
+-- 
+-- INSERT INTO Askare(name, description, lisatty, deadline, tarkeysaste) VALUES ('Tee tiskit', 'Yhdessä aiheutettujen tiskien tiskaaminen käsin. Luo harmoniaa.', NOW(), '2017-30-03', 1);
+-- Player-taulun testidata
+INSERT INTO Player (name, password) VALUES ('Kalle', 'Kalle123'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
+INSERT INTO Player (name, password) VALUES ('Henri', 'Henri123');
+-- Game taulun testidata
+INSERT INTO Game (name, description, published, publisher, added) VALUES ('The Elder Scrolls V: Skyrim', 'Arrow to the knee', '2011-11-11', 'Bethesda Softworks', NOW());
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,18 +1,32 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
-CREATE TABLE Kayttaja(
-id SERIAL PRIMARY KEY,
-name varchar(50) NOT NULL,
-password varchar(50) NOT NULL
+-- CREATE TABLE Kayttaja(
+-- id SERIAL PRIMARY KEY,
+-- name varchar(50) NOT NULL,
+-- password varchar(50) NOT NULL
+-- );
+-- 
+-- CREATE TABLE Askare(
+-- id SERIAL PRIMARY KEY,
+-- kayttaja_id INTEGER REFERENCES Kayttaja(id),
+-- name varchar(50) NOT NULL,
+-- suoritettu boolean DEFAULT FALSE,
+-- description varchar(400),
+-- lisatty DATE,
+-- deadline DATE,
+-- tarkeysaste INTEGER
+-- );
+CREATE TABLE Player(
+  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  name varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+  password varchar(50) NOT NULL
 );
 
-CREATE TABLE Askare(
-id SERIAL PRIMARY KEY,
-kayttaja_id INTEGER REFERENCES Kayttaja(id),
-name varchar(50) NOT NULL,
-suoritettu boolean DEFAULT FALSE,
-description varchar(400),
-lisatty DATE,
-deadline DATE,
-tarkeysaste INTEGER
-);
-    
+CREATE TABLE Game(
+  id SERIAL PRIMARY KEY,
+  player_id INTEGER REFERENCES Player(id), -- Viiteavain Player-tauluun
+  name varchar(50) NOT NULL,
+  played boolean DEFAULT FALSE,
+  description varchar(400),
+  published DATE,
+  publisher varchar(50),
+  added DATE
