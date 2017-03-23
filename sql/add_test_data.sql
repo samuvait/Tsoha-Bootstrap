855461433b523575a8e4a373839b3a16581b418a
samuvait <samu.vaittinen@helsinki.fi>
2017-03-23 23:43:42 +0200

    datan lisays

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,2 +1,4 @@
 INSERT INTO Kayttaja (name, password) VALUES ('Pekka', 'Pekka123');
 INSERT INTO Kayttaja (name, password) VALUES ('Maija', 'Maija123');
+
+INSERT INTO Askare (name, description, deadline, lisatty) VALUES ('Tee tiskit', 'Yhdessä aiheutettujen tiskien tiskaaminen käsin. Luo harmoniaa.', '2017-04-04', NOW());
