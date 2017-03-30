c3430d60fd17b5b092e2a4d1225362ecee7f5660
samuvait <samu.vaittinen@helsinki.fi>
2017-03-30 22:05:15 +0300

    tietokantoja lisatty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,4 +1,7 @@
 INSERT INTO Kayttaja (name, password) VALUES ('Pekka', 'Pekka123');
 INSERT INTO Kayttaja (name, password) VALUES ('Maija', 'Maija123');
 
-INSERT INTO Askare (name, luokka, description, deadline, importance, added) VALUES ('Tee tiskit', 'Kotityöt','Yhdessä aiheutettujen tiskien tiskaaminen käsin. Luo harmoniaa.', '2017-04-04', '10', NOW());
+INSERT INTO Luokka (luokka_name, description) VALUES ('Kotityöt', 'Asunnosta huolehtimiseen tarvittavat askareet, joista kaikki ovat yhteisesti vastuussa.');
+
+INSERT INTO Askare (name, luokka, description, deadline, importance, added) VALUES ('Tee tiskit', 'Kotityöt','Yhdessä aiheutettujen tiskien tiskaaminen käsin. Luo harmoniaa.', '2017-04-14', '10', NOW());
+INSERT INTO Askare (name, luokka, description, deadline, importance, added) VALUES ('Imurointi', 'Kotityöt','Talouden imurointi läpikotaisin', '2017-04-10', '8', NOW());
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,9 +4,15 @@ name varchar(50) NOT NULL,
 password varchar(50) NOT NULL
 );
 
+CREATE TABLE Luokka(
+luokka_id SERIAL PRIMARY KEY,
+luokka_name varchar(50) NOT NULL,
+description varchar(400)
+);
+
 CREATE TABLE Askare(
 id SERIAL PRIMARY KEY,
--- kayttaja_id INTEGER REFERENCES Kayttaja(id),
+kayttaja_id INTEGER REFERENCES Kayttaja (id),
 name varchar(50) NOT NULL,
 luokka varchar(40),
 done varchar(20),
@@ -14,4 +20,10 @@ description varchar(400),
 added DATE,
 deadline DATE,
 importance INTEGER
+);
+
+CREATE TABLE Askare_luokka(
+askare_id integer REFERENCES Askare (id) ON UPDATE CASCADE ON DELETE CASCADE,
+luokka_id integer REFERENCES Luokka (luokka_id) ON UPDATE CASCADE,
+CONSTRAINT askare_luokka_pkey PRIMARY KEY (askare_id, luokka_id)
 );
