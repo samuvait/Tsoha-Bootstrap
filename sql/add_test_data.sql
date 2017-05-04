4c91b7fbd2cd58c362bc7890222cda4236d81512
samuvait <samu.vaittinen@helsinki.fi>
2017-05-04 23:10:16 +0300

    testidata korjattu

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -5,11 +5,11 @@ INSERT INTO Luokka (luokka_name, description, kayttaja_id) VALUES ('Kotityöt', 
 INSERT INTO Luokka (luokka_name, description, kayttaja_id) VALUES ('Kotitehtävät', 'Kurssien tehtävät, jotka täytyy tehdä, jotta menestyy kurssilla.', '1');
 INSERT INTO Luokka (luokka_name, description, kayttaja_id) VALUES ('Lemmikkieläimet', 'Lemmikkieläinten huolehtimisen askareet.', '2');
 
-INSERT INTO Askare (name, luokka, description, deadline, importance, added, kayttaja_id) VALUES ('Tee tiskit', 'Kotityöt','Yhdessä aiheutettujen tiskien tiskaaminen käsin. Luo harmoniaa.', '2017-05-14', '10', NOW(), '1');
-INSERT INTO Askare (name, luokka, description, deadline, importance, added, kayttaja_id) VALUES ('Imurointi', 'Kotityöt','Talouden imurointi läpikotaisin', '2017-05-12', '8', NOW(), '1');
+INSERT INTO Askare (name, description, deadline, importance, added, kayttaja_id) VALUES ('Tee tiskit', 'Yhdessä aiheutettujen tiskien tiskaaminen käsin. Luo harmoniaa.', '2017-05-14', '10', NOW(), '1');
+INSERT INTO Askare (name, description, deadline, importance, added, kayttaja_id) VALUES ('Imurointi', 'Talouden imurointi läpikotaisin', '2017-05-12', '8', NOW(), '1');
 
-INSERT INTO Askare (name, luokka, description, deadline, importance, added, kayttaja_id) VALUES ('Kissan pesu', 'Lemmikkieläimet', 'Kissa täytyy pestä sen vastalauseista huolimatta', '2017-05-16', '15', NOW(), '2');
-INSERT INTO Askare (name, luokka, description, deadline, importance, added, kayttaja_id) VALUES ('Koiran ulkoiluttaminen', 'Lemmikkieläimet','Koiran vieminen ulos ja sen kävelyttäminen', '2017-05-15', '9', NOW(), '2');
+INSERT INTO Askare (name, description, deadline, importance, added, kayttaja_id) VALUES ('Kissan pesu', 'Kissa täytyy pestä sen vastalauseista huolimatta', '2017-05-16', '15', NOW(), '2');
+INSERT INTO Askare (name, description, deadline, importance, added, kayttaja_id) VALUES ('Koiran ulkoiluttaminen', 'Koiran vieminen ulos ja sen kävelyttäminen', '2017-05-15', '9', NOW(), '2');
 
 INSERT INTO Askare_luokka (askare_id, luokka_id) VALUES (1, 1);
 INSERT INTO Askare_luokka (askare_id, luokka_id) VALUES (2, 1);
